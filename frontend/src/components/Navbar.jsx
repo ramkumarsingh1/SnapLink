7efd2b9c61aaea039cff535074dc34f6db2c7fb4
Ram Kumar <tharabandhuram@gmail.com>
2026-07-19 23:26:48 +0530

    navbar m advanced or escap ki functionality add kiya gya hai

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,15 +1,41 @@
 import React, { useState, useEffect } from 'react'
-import { HiOutlineBell, HiOutlineMenu, HiOutlineUser, HiOutlineX } from "react-icons/hi";
+import { HiOutlineBell, HiOutlineInformationCircle, HiOutlineMenu, HiOutlineUser, HiOutlineX } from "react-icons/hi";
+
+
+const guestMenu = [
+    {
+        id: 1,
+        title: "Login",
+        icon: HiOutlineUser,
+    },
+];
+const menuItems = guestMenu;
 export default function Navbar() {
     const [menuOpen, setMenuOpen] = useState(false);
 
     useEffect(()=>{
-        if(menuOpen){
-            document.body.style.overflow="hidden";
-        }else{
-            document.body.style.overflow="auto";
+        const handleKeyDown =(event) =>{
+            console.log(event.key);
+            if(event.key === "Escape" && menuOpen){
+                setMenuOpen(false);
+            }
         }
+        window.addEventListener("keydown",handleKeyDown);
+        return ()=>{
+            window.removeEventListener("keydown", handleKeyDown);
+        };
     },[menuOpen])
+    useEffect(() => {
+        if (menuOpen) {
+            document.body.style.overflow = "hidden";
+        } else {
+            document.body.style.overflow = "auto";
+        }
+        // agar kisi wajh se navbar component remove ho jaye to user scroll kar paye
+        return () => {
+            document.body.style.overflow = "auto";
+        }
+    }, [menuOpen])
 
     return (
         <>
@@ -36,38 +62,50 @@ export default function Navbar() {
             </div>
 
             {/* Mobile Menu yahan aayega */}
-                <div className={`fixed inset-0 md:hidden ${menuOpen ? 'bg-black/40':'pointer-events-none'}`}
-                onClick={()=> setMenuOpen(false)}
-                >
-                <div 
-                onClick={()=>setMenuOpen(false)}
-                className='fixed inset-0 bg-black/40 md:hidden'>
-                    <div 
-                    
-                    onClick={(e)=> e.stopPropagation()}
-                    className={`ml-auto h-full w-72 bg-white shadow-xl transition-transform duration-300 ${menuOpen ? 'translate-x-0': 'translate-x-full'}`}>
-                    {/* <div className='md:hidden bg-blue-500 border-t border-gray-200 shadow-md'>
+            <div className={`fixed inset-0 md:hidden ${menuOpen ? 'bg-black/40' : 'pointer-events-none'}`}
+                onClick={() => setMenuOpen(false)}
+            >
+                <div
+                    onClick={() => setMenuOpen(false)}
+                    className='fixed inset-0 bg-black/40 md:hidden'>
+                    <div
+                        onClick={(e) => e.stopPropagation()}
+                        className={`ml-auto h-full w-72 bg-white shadow-xl flex flex-col transition-transform duration-300 ${menuOpen ? 'translate-x-0' : 'translate-x-full'}`}>
+                        {/* <div className='md:hidden bg-blue-500 border-t border-gray-200 shadow-md'>
                         <button className='block w-full text-left px-4 py-3 hover:bg-gray-100'>
 
                             Login
                         </button>
                     </div> */}
-                    <div className='flex items-center justify-between p-4 border-b border-gray-200'>
-                    <h2 className="text-lg font-semibold">SnapLink</h2>
-                    <button
-                    onClick={()=> setMenuOpen(false)}
-                    className='p-2 rounded-lg hover:bg-gray-100 transition'
-                    >
-                        <HiOutlineX className='text-3xl text-gray-700' />
-                    </button>
-                    </div>
-                    <div className='p-2'>
-                        <button className='w-full flex items-center rounded-lg px-4 py-3 bg-blue-300 hover:bg-blue-400 transition '>
-                            <HiOutlineUser className='text-xl text-gray-700'/>
-                            <span className='ml-3 font-medium text-gray-700'>Login</span>
-                            
-                        </button>
-                    </div>
+                        <div className='flex items-center justify-between p-4 border-b border-gray-200'>
+                            <h2 className="text-lg font-semibold">SnapLink</h2>
+                            <button
+                                onClick={() => setMenuOpen(false)}
+                                className='p-2 rounded-lg hover:bg-gray-100 transition'
+                            >
+                                <HiOutlineX className='text-3xl text-gray-700' />
+                            </button>
+                        </div>
+                        <div className='p-2 flex-1'>
+                            {menuItems.map((item) => {
+                                const Icon = item.icon;
+                                return (
+
+                                    <button
+                                        key={item.id}
+                                        className='w-full flex items-center rounded-lg px-4 py-3 bg-white hover:bg-gray-400 transition '>
+
+                                        <Icon className='text-xl text-gray-700' />
+                                        <span className='ml-3 font-medium text-gray-700'>{item.title}</span>
+
+                                    </button>
+                                )
+                            })}
+
+                        </div>
+                        <div className='border-t border-gray-200 p-4'>
+                            <p className='text-center text-sm text-gray-500'>SnapLink v1.0</p>
+                        </div>
                     </div>
                 </div>
             </div>
